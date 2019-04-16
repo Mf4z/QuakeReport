a747bf7ea902f687b3f7c59bfb687a8272819b39
Marwan <emef4z@gmail.com>
2019-04-16 17:20:07 +0100

    Add JSON object and list generated from JSON

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -33,15 +33,7 @@ public class EarthquakeActivity extends AppCompatActivity {
 
 
         // Create a fake list of earthquake locations.
-        final ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("2.1","San Francisco","Feb 3, 2015"));
-        earthquakes.add(new Earthquake("3.1","London","Feb 4, 2015"));
-        earthquakes.add(new Earthquake("4.1","Tokyo","Feb 5, 2015"));
-        earthquakes.add(new Earthquake("5.1","Mexico City","Feb 6, 2015"));
-        earthquakes.add(new Earthquake("4.1","Tokyo","Feb 5, 2015"));
-        earthquakes.add(new Earthquake("5.1","Moscow","Feb 6, 2015"));
-        earthquakes.add(new Earthquake("4.5","Rio de Janeiro","Feb 7, 2015"));
-        earthquakes.add(new Earthquake("6.1","Paris","Feb 8, 2015"));
+        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
