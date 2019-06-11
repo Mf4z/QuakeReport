9095ac8acdd8918ed157daf5d1825f00e0f17065
Marwan <emef4z@gmail.com>
2019-06-11 15:38:28 +0100

    Test for internet connection and change empty state msg

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,7 +16,10 @@
 package com.example.android.quakereport;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.content.Loader;
 import android.os.Bundle;
@@ -48,8 +51,6 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     /** TextView that is displayed when the list is empty */
     private TextView mEmptyStateTextView;
 
-    //Progress bar to be shown when loading data
-    private ProgressBar progressBar;
 
     /** Sample JSON response for a USGS query */
     private String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
@@ -83,9 +84,6 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
 
         Log.e(LOG_TAG,"initLoader");
-
-        //Initialised progress bar
-        progressBar = findViewById(R.id.loading_spinner);
 
         //Initialised empty text view
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
@@ -125,12 +123,15 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         // Clear the adapter of previous earthquake data
         Log.e(LOG_TAG,"onLoadFinished");
 
-        //Set progress bar visibility
-        progressBar.setVisibility(View.GONE);
 
-        // Set empty state text to display "No earthquakes found."
-        mEmptyStateTextView.setText(R.string.no_earthquakes);
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
 
+        if(!checkConnectivity(this)) {
+            // Set empty state text to display "No earthquakes found."
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
         mAdapter.clear();
 
         // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
@@ -146,6 +147,19 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         // Loader reset, so we can clear out our existing data.
         Log.e(LOG_TAG,"onLoaderReset");
         mAdapter.clear();
+    }
+
+    public boolean checkConnectivity(Context context){
+
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+     boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+     return isConnected;
+
     }
 
 }
